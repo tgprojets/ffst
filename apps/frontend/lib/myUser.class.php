ae0ae32bbf44773e174780e4d5d8702d162794df
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-07-04 09:32:45 +0200

    Refs #401 Integration HTML & data fixtures permissions

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -1,5 +1,14 @@
 <?php
 
-class myUser extends sfBasicSecurityUser
+class myUser extends sfGuardSecurityUser
 {
+    public function isFirstRequest($boolean = null)
+    {
+      if (is_null($boolean))
+      {
+        return $this->getAttribute('first_request', true);
+      }
+
+      $this->setAttribute('first_request', $boolean);
+    }
 }
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -9,6 +9,54 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="conteneur">
+      <div class="sb_bandeau">
+          <div class="sb_logo">
+            <img src="images/logo.png" alt="" />
+          </div>
+          <div class="sb_titre">
+            <h1>FFST</h1>
+            <?php if ($sf_user->isAuthenticated()): ?>
+            <div class="sb_bar_menu_connexion">
+                <div class="sb_deconnexion">
+                  <?php echo 'Bonjour '.$sf_user->getGuardUser()->getFirstName().' '.$sf_user->getGuardUser()->getLastName() ?>
+                </div>
+                <?php echo link_to('&nbsp;', '@sf_guard_signout', array('class' => 'sb_deconnexion_btn')) ?>
+              <?php if (isset($gsMessage)): ?>
+                <div class="sb_notice"><?php echo $gsMessage ?></div>
+              <?php endif; ?>
+            </div>
+            <?php endif; ?>
+            <div class="sb_bar">
+              <div class="sb_menu">
+                <?php if ($sf_user->isAuthenticated()): ?>
+                  <div class="sb_bord_menu_gauche">
+                  </div>
+                  <ul id="sb_nav">
+                      <li <?php echo $sf_params->get('module')=="main"?'class="sb_menu_select"':''?>><?php echo link_to('Accueil', '@homepage') ?></li>
+                  </ul>
+                  <div class="sb_bord_menu_droite">
+                  </div>
+                <?php endif; ?>
+              </div>
+              <div class="sb_bar_menu_connexion">
+              </div>
+            </div>
+          </div>
+      </div>
+      <div id="contenant">
+        <div class="sb_centre">
+            <div class="sb_content">
+               <?php echo $sf_content ?>
+            </div>
+        </div>
+        <div id="margepied"><!-- ne pas enlever cette marge et laisser en dernier --></div>
+      </div>
+    </div>
+    <div class="sb_baspage">
+      <?php //echo link_to('Copyright', 'main/copyright') ?> - Thomas GILBERT -
+      <?php //echo link_to('Apropos de', 'main/aproposde') ?>
+    </div>
   </body>
+
 </html>
