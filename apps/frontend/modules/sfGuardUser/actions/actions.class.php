0612c24321c1c513ed14b1ff77ccd7dda37bc340
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-07-04 20:33:35 +0200

    Refs #403 add module users"

---
+++ apps/frontend/modules/sfGuardUser/actions/actions.class.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/sfGuardUserGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/sfGuardUserGeneratorHelper.class.php';
+
+/**
+ * sf_guard_user actions.
+ *
+ * @package    sfGuardPlugin
+ * @subpackage sf_guard_user
+ * @author     Thomas GILBERT <tgilbert@tgprojets.fr>
+ * @version    1.0 GIT
+ */
+class sfGuardUserActions extends autoSfGuardUserActions
+{
+  public function executeEditpassword(sfWebRequest $request) {
+    $this->form = new ModifpasswordForm();
+    if ($request->isMethod('post')) {
+        $this->form->bind($request->getParameter('modifpassword'));
+        if ($this->form->isValid())
+        {
+           $aValues = $this->form->getValues();
+           $this->getUser()->setPassword($aValues['password_forgot']);
+
+           $this->redirect('main/index');
+        }
+    }
+  }
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+    if ($form->isValid())
+    {
+
+      $notice = $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.';
+      try {        
+        $sf_guard_user = $form->save();
+      } catch (Doctrine_Validator_Exception $e) {
+
+        $errorStack = $form->getObject()->getErrorStack();
+
+        $message = get_class($form->getObject()) . ' has ' . count($errorStack) . " field" . (count($errorStack) > 1 ?  's' : null) . " with validation errors: ";
+        foreach ($errorStack as $field => $errors) {
+            $message .= "$field (" . implode(", ", $errors) . "), ";
+        }
+        $message = trim($message, ', ');
+
+        $this->getUser()->setFlash('error', $message);
+        return sfView::SUCCESS;
+      }
+
+      $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $sf_guard_user)));
+
+      if ($request->hasParameter('_save_and_add'))
+      {
+        $this->getUser()->setFlash('notice', $notice.' You can add another one below.');
+
+        $this->redirect('@sf_guard_user_new');
+      } elseif ($request->hasParameter('_save_and_list')) {
+          $this->getUser()->setFlash('notice', $this->getUser()->getFlash('notice') );
+          $this->redirect('@sf_guard_user');
+      }
+      else
+      {
+        $this->getUser()->setFlash('notice', $notice);
+
+        $this->redirect(array('sf_route' => 'sf_guard_user_edit', 'sf_subject' => $sf_guard_user));
+      }
+    }
+    else
+    {
+      $this->getUser()->setFlash('error', 'The item has not been saved due to some errors.', false);
+    }
+  }
+
+}
---
+++ apps/frontend/modules/sfGuardUser/templates/editpasswordSuccess.php
@@ -0,0 +1,44 @@
+<?php use_stylesheet('formulaire_connexion.css') ?>
+<div class="noname_connexion">
+<h1><?php echo __('Edit your password') ?></h1>
+
+<form action="<?php echo url_for('sfGuardUser/editpassword'); ?>" method="post" id="myform" height="250px">
+    <fieldset>
+        
+		<ul class='formRegister'>
+
+			<?php if ($form->hasGlobalErrors()): ?>
+	        <?php foreach ($form->getGlobalErrors() as $name => $error): ?>
+	          <li class='error'><?php echo $error ?></li>
+	        <?php endforeach; ?>
+			<?php endif; ?>
+
+			<?php foreach ($form as $widget): ?>
+
+			<?php if ($widget->hasError()): ?>
+			   <li class='error'><?php echo $widget->getError(); ?></li>
+			<?php endif; ?>
+
+			<?php if (!$widget->isHidden()) { ?>
+			 <li>
+			 	<span>
+			 		<?php echo $widget->renderLabel(); ?>
+				</span>
+			 	<?php echo $widget->render() ?>
+                                <?php if ($widget->renderHelp()): ?>
+                                  <div class="noname_form_help" title="<?php echo $widget->renderHelp() ?>"></div>
+                                <?php endif; ?>
+			 </li>
+			 <?php } else { ?>
+			 	<li style="height: 1px;"><?php echo $widget->render() ?></li>
+			 <?php } ?>
+			<?php endforeach; ?>
+		</ul>
+
+        <div class="noname_center">
+            <?php echo button_to(__('Cancel'), '@homepage', array('class' => 'icone_cancel noname_button')) ?>
+            <input type="submit" class='icone_valider noname_button' value=<?php echo __("Validate") ?> />
+        </div>
+    </fieldset>
+</form>
+</div>
