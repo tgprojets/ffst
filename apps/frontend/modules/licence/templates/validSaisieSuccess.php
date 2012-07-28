3905644f81cc445dc08fe267dc1aefb37ddbe539
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-07-28 16:43:54 +0200

    Refs #420 calc cout licence & valid saisie

---
+++ apps/frontend/modules/licence/templates/validSaisieSuccess.php
@@ -0,0 +1,17 @@
+<?php if (isset($oPaymentLic)): ?>
+    <?php foreach($oPaymentLic as $oPaiement): ?>
+        <?php include_partial('licence/displayBordereau', array('oElement' => $oPaiement)) ?>
+    <?php endforeach; ?>
+
+    <?php foreach($oPaymentClub as $oPaiement): ?>
+        <?php include_partial('licence/displayBordereau', array('oElement' => $oPaiement)) ?>
+    <?php endforeach; ?>
+
+    <?php foreach($oAvoirLic as $oAvoir): ?>
+        <?php include_partial('licence/displayBordereau', array('oElement' => $oAvoir)) ?>
+    <?php endforeach; ?>
+
+    <?php foreach($oAvoirClub as $oAvoir): ?>
+        <?php include_partial('licence/displayBordereau', array('oElement' => $oAvoir)) ?>
+    <?php endforeach; ?>
+<?php endif; ?>
