7a5ff7755ec9d3645ce52f9172f4ff12687489d9
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-08-31 10:41:56 +0200

    Synch mysql

--- synchbddmysql.sh
+++ synchbddmysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    ./symfony doctrine:build --all --no-confirmation
+    #./symfony doctrine:build --all --no-confirmation
     bdd="ffst_dev"
 elif [ "$1" == "prod" ]; then
     ./symfony doctrine:build --all --no-confirmation --env="$1"
@@ -12,19 +12,19 @@ else
     ./symfony doctrine:build --all --no-confirmation --env="$1"
     bdd="ffst_$1"
 fi
-./symfony doctrine:data-load data/fixtures/fixtures.yml
+#./symfony doctrine:data-load data/fixtures/fixtures.yml
 
 codepostaux="data/patch/codepostaux.sql"
 
 if [ "$1" == "prod" ]; then
-    export PGUSER=ffst;
-    export PGPASSWORD=NicudOaltij7;
+    sUSER=ffst
+    sPASSWORD=NicudOaltij7
 elif [ "$1" == "sb" ]; then
-    export PGUSER=ffst_sb;
-    export PGPASSWORD=pho4raCh;
+    sUSER=ffst_sb
+    sPASSWORD=pho4raCh
 else
-    export PGUSER=userdev;
-    export PGPASSWORD=dev_projets;
+    sUSER="userdev"
+    sPASSWORD="dev_projets"
 fi
 
-psql -h localhost -d $bdd -f $codepostaux
+mysql -u $sUSER --password=${sPASSWORD} -h localhost -D ${bdd} < $codepostaux
