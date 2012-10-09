f3a6c4ea1f8a9064065374d64e5ee6c75b8652b2
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-10-09 17:47:59 +0200

    Refs #451 Add params certificat

---
+++ apps/frontend/modules/tbl_params/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/tbl_paramsGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/tbl_paramsGeneratorHelper.class.php';
+
+/**
+ * tbl_params actions.
+ *
+ * @package    sf_sandbox
+ * @subpackage tbl_params
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class tbl_paramsActions extends autoTbl_paramsActions
+{
+}
---
+++ lib/filter/doctrine/tbl_paramsFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * tbl_params filter form.
+ *
+ * @package    sf_sandbox
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class tbl_paramsFormFilter extends Basetbl_paramsFormFilter
+{
+  public function configure()
+  {
+  }
+}
