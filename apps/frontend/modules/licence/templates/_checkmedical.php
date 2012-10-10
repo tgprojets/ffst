1cd7d4bcf874bff639a1166722f8bb9509cc177a
Thomas GILBERT <tgilbert@tgprojets.fr>
2012-10-10 10:39:19 +0200

    Refs #452 Add fields doctor

--- apps/frontend/modules/licence/templates/_checkmedical.php
+++ apps/frontend/modules/licence/templates/_checkmedical.php
@@ -4,17 +4,29 @@
         if ($('#tbl_licence_is_medical').attr("checked") == undefined)
         {
             $('.sf_admin_form_field_date_medical').hide();
+            $('.sf_admin_form_field_lastname_doctor').hide();
+            $('.sf_admin_form_field_firstname_doctor').hide();
+            $('.sf_admin_form_field_rpps').hide();
         }
         $('#tbl_licence_is_medical').click(function () {
             if ($('#tbl_licence_is_medical').attr("checked") == undefined)
             {
                 $('.sf_admin_form_field_date_medical').hide();
+                $('.sf_admin_form_field_lastname_doctor').hide();
+                $('.sf_admin_form_field_firstname_doctor').hide();
+                $('.sf_admin_form_field_rpps').hide();
+                $('#tbl_licence_lastname_doctor').val('');
+                $('#tbl_licence_firstname_doctor').val('');
+                $('#tbl_licence_rpps').val('');
                 $('#tbl_licence_date_medical_day').val('');
                 $('#tbl_licence_date_medical_month').val('');
                 $('#tbl_licence_date_medical_year').val('');
             } else {
                 alert('J\'atteste sur l\'honneur du contrôle et de la validité du certificat médical. En cas de fausse déclaration, je suis informé que j\'engagerai ma responsabilité civile et/ou pénale et m\'exposerai aux poursuites prévues par la Loi.');
                 $('.sf_admin_form_field_date_medical').show();
+                $('.sf_admin_form_field_lastname_doctor').show();
+                $('.sf_admin_form_field_firstname_doctor').show();
+                $('.sf_admin_form_field_rpps').show();
             }
         });
     });
